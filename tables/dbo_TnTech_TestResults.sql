814e1c44865d84c29996f65cbbbf6d525e91349d
Robert Bingham <rbbingham1893@gmail.com>
2024-02-06 20:14:15 -0600

    removed delimit on variables

--- tables/dbo_TnTech_TestResults.sql
+++ tables/dbo_TnTech_TestResults.sql
@@ -5,16 +5,15 @@ GO
 -- TODO: convert datetime to date
 -- TODO: ID is a weak identifier, find another solution
 CREATE TABLE [dbo].[TnTech_TestResults](
-	[ID] [bigint] IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED ([ID] ASC),
-	[CreatedOn] [date] NOT NULL CONSTRAINT [DF_TnTech_TestResults_CreatedOn] DEFAULT(GETDATE()),
-	[CreatedBy] [varchar](256) NULL,
-	[ModifiedOn] [date] NULL,
-	[ModifiedBy] [varchar](256) NULL,
-	[TableName] [varchar](256) NOT NULL,
-	[TestName] [varchar](256) NOT NULL,
-	[TestRunDate] [date] NOT NULL,
-	[ActualResult] [bigint] NOT NULL,
-	[ExpectedResult] [bigint] NULL,
-	[Completed] [bit] NOT NULL,
+	[ID] bigint IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED ([ID] ASC),
+	[TableName] varchar(256) NOT NULL,
+	[TestRunDate] date NOT NULL,
+	[TestName] varchar(256) NOT NULL,
+	[ActualResult] bigint NOT NULL,
+	[ExpectedResult] bigint NULL,
+	[CreatedOn] date NOT NULL CONSTRAINT [DF_TnTech_TestResults_CreatedOn] DEFAULT(GETDATE()),
+	[CreatedBy] varchar(256) NULL,
+	[ModifiedOn] date NULL,
+	[ModifiedBy] varchar(256) NULL,
 )
 GO
