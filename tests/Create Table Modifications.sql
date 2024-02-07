d1ac21d19479f71a7e874bfc38b964012b55b4b6
Robert Bingham <rbbingham1893@gmail.com>
2024-02-06 20:03:38 -0600

    re-ordered the columns

--- tests/Create Table Modifications.sql
+++ tests/Create Table Modifications.sql
@@ -3,24 +3,16 @@ GO
 
 CREATE TABLE [dbo].[TnTech_TestResults](
 	[ID] bigint IDENTITY(1,1) NOT NULL CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED ([ID] ASC),
-	[CreatedOn] date NOT NULL CONSTRAINT [DF_TnTech_TestResults_CreatedOn] DEFAULT(GETDATE()),
-	[CreatedBy] varchar(256) NULL,
+	[TableName] varchar(256) NOT NULL,
 	[TestRunDate] datetime NULL,
 	[TestName] varchar(256)NOT NULL,
-	[ModifiedOn] datetime NULL,
-	[ModifiedBy] varchar(256) NULL,
-	[TableName] varchar(256) NOT NULL,
 	[ActualResult] bigint NOT NULL,
 	[ExpectedResult] bigint NULL,
-	[Completed] bit NOT NULL,
- CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED 
-(
-	[ID] ASC --ID is a weak Primary key
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-
-ALTER TABLE [dbo].[TnTech_TestResults] ADD  CONSTRAINT [DF_TnTech_TestResults_CreatedOn]  DEFAULT (getdate()) FOR [CreatedOn]
+	[CreatedOn] date NOT NULL CONSTRAINT [DF_TnTech_TestResults_CreatedOn] DEFAULT(GETDATE()),
+	[CreatedBy] varchar(256) NULL,
+	[ModifiedOn] datetime NULL,
+	[ModifiedBy] varchar(256) NULL,
+)
 GO
 
 
