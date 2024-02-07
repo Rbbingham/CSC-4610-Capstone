a56bc64e44aba26d5d9c15533004987506109815
Robert Bingham <rbbingham1893@gmail.com>
2024-02-06 20:02:46 -0600

    modified to fit Transcard's standards

--- tests/Create Table Modifications.sql
+++ tests/Create Table Modifications.sql
@@ -2,8 +2,8 @@ USE [CapstoneDB]
 GO
 
 CREATE TABLE [dbo].[TnTech_TestResults](
-	[ID] bigint IDENTITY(1,1) NOT NULL,
-	[CreatedOn] date NOT NULL,
+	[ID] bigint IDENTITY(1,1) NOT NULL CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED ([ID] ASC),
+	[CreatedOn] date NOT NULL CONSTRAINT [DF_TnTech_TestResults_CreatedOn] DEFAULT(GETDATE()),
 	[CreatedBy] varchar(256) NULL,
 	[TestRunDate] datetime NULL,
 	[TestName] varchar(256)NOT NULL,
