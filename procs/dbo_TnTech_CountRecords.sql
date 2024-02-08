de6eef49e5c3a170413e174de7826fbca77ec2f7
Robert Bingham <rbbingham1893@gmail.com>
2024-02-08 16:16:46 -0500

    moved queires to sep file; droped query test.sql and table TnTech_RecordCounts

--- procs/dbo_TnTech_CountRecords.sql
+++ procs/dbo_TnTech_CountRecords.sql
@@ -76,32 +76,3 @@ BEGIN
 	SET NOCOUNT OFF;
 END;
 GO
-
-DECLARE @TodaysDate datetime;
-SET @TodaysDate = GETDATE();
-EXEC [dbo].[TnTech_CountRecords] @TableName = '[BI_Feed].[dbo].[BI_BankCore_Products]', 
-								 @Column = '[ProductId]', 
-								 @Date = @TodaysDate,
-								 @Expected = 5;
-
-EXEC [dbo].[TnTech_CountRecords] @TableName = '[BI_Feed].[dbo].[BI_BDA_Institutions]', 
-								 @Column = '[institutionId]', 
-								 @Date = @TodaysDate,
-								 @Expected = 20;
-
-EXEC [dbo].[TnTech_CountRecords] @TableName = '[BI_Feed].[dbo].[BI_BDA_UniqueProducts]', 
-								 @Column = '[ID]', 
-								 @Date = @TodaysDate,
-								 @Expected = 3850;
-
-EXEC [dbo].[TnTech_CountRecords] @TableName = '[BI_Feed].[dbo].[BI_ProductInclusionTables]', 
-								 @Column = '[MerchantGroup]', 
-								 @Date = @TodaysDate,
-								 @Expected = 60;
-
-EXEC [dbo].[TnTech_CountRecords] @TableName = '[BI_Feed].[dbo].[Toyota_Distribution]', 
-								 @Column = '[Vin]', 
-								 @Date = @TodaysDate,
-								 @Expected = '13000';
-
-SELECT * FROM [CapstoneDB].[dbo].[TnTech_TestResults] ORDER BY ID DESC;
