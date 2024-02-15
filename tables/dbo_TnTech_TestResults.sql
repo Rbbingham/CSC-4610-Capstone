a659ec79dbfc3f93e1ad63f7bf03687001abb7a0
Robert Bingham <rbbingham1893@gmail.com>
2024-02-15 16:12:57 -0500

    updated [dbo].[TnTech_TestResults] to fit with standards

--- tables/dbo_TnTech_TestResults.sql
+++ tables/dbo_TnTech_TestResults.sql
@@ -1,18 +1,22 @@
 USE [CapstoneDB]
 GO
 
--- TODO: convert datetime to date
--- TODO: ID is a weak identifier, find another solution
+IF OBJECT_ID('CapstoneDB.dbo.TnTech_TestResults') IS NOT NULL
+BEGIN
+	DROP TABLE [CapstoneDB].[dbo].[TnTech_TestResults];
+END
+
 CREATE TABLE [dbo].[TnTech_TestResults](
-	[ID] bigint IDENTITY(1, 1) NOT NULL CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED ([ID] ASC),
 	[TableName] varchar(256) NOT NULL,
 	[TestRunDate] date NOT NULL,
 	[TestName] varchar(256) NOT NULL,
 	[ActualResult] bigint NOT NULL,
-	[ExpectedResult] bigint NULL,
+	[ExpectedResult] bigint NOT NULL,
+	[Deviation] bigint NULL,
 	[CreatedOn] date NOT NULL CONSTRAINT [DF_TnTech_TestResults_CreatedOn] DEFAULT(GETDATE()),
-	[CreatedBy] varchar(256) NULL,
+	[CreatedBy] varchar(256) NOT NULL,
 	[ModifiedOn] date NULL,
 	[ModifiedBy] varchar(256) NULL,
+	CONSTRAINT [PK_TnTech_TestResults] PRIMARY KEY CLUSTERED ([TestRunDate], [TableName], [TestName] ASC),
 )
 GO
