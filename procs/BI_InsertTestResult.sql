2303df7d3f56c66e0b84699fbcc9983e50972aaf
owlthechill <36138074+owlthechill@users.noreply.github.com>
2024-02-16 12:51:50 -0600

    Update and rename Test Insert.sql to BI_InsertTestResult.sql

---
+++ procs/BI_InsertTestResult.sql
@@ -0,0 +1,47 @@
+CREATE OR ALTER PROCEDURE [dbo].[BI_InsertTestResult]
+	@TableName VARCHAR(256),
+	@TestRunDate DATE,
+	@TestName VARCHAR(256),
+	@ActualResult BIGINT,
+	@ExpectedResult BIGINT,
+	@Deviation BIGINT,
+	@CreatedOn DATETIME,
+	@CreatedBy VARCHAR(256),
+	@ModifiedOn DATETIME,
+	@ModifiedBy VARCHAR(256)
+AS
+BEGIN
+	SET NOCOUNT ON;
+
+	IF EXISTS ( SELECT * FROM CapstoneDB.dbo.BI_HealthResults WHERE TableName = @TableName AND TestName = @TestName AND TestRunDate = @TestRunDate)
+	BEGIN
+		UPDATE CapstoneDB.dbo.BI_HealthResults
+		SET [CreatedOn] = @CreatedOn,
+			[TestRunDate] = @TestRunDate,
+			[ActualResult] = @ActualResult,
+			[ExpectedResult] = @ExpectedResult,
+			[Deviation] = @Deviation,
+			[CreatedBy] = @CreatedBy,
+			[ModifiedOn] = @ModifiedOn,
+			[ModifiedBy] = @ModifiedBy
+		WHERE [TableName] = @TableName AND [TestName] = @TestName AND [TestRunDate] = @TestRunDate;
+	END
+
+	ELSE
+
+	BEGIN
+		INSERT INTO CapstoneDB.dbo.BI_HealthResults (TableName, TestRunDate, TestName, ActualResult, ExpectedResult, Deviation, CreatedOn, CreatedBy, ModifiedOn, ModifiedBy)
+		VALUES (
+			@TableName,
+			@TestRunDate,
+			@TestName,
+			@ActualResult,
+			@ExpectedResultm
+			@Deviation,
+			@CreatedOn,
+			@CreatedBy,
+			@ModifiedOn,
+			@ModifiedBy
+			);
+	END
+END;
