a846c36d99a706fc4a942caec51541a218b596d7
Carlos Escudero <Carlos.Escudero@transcard.com>
2024-02-20 16:49:09 -0500

    Pushing proc modification

--- procs/BI_ProductInclusionTables Proc.sql
+++ procs/BI_ProductInclusionTables Proc.sql
@@ -77,9 +77,9 @@ BEGIN
 	FROM 
 		BI_Feed.dbo.BI_ProductInclusionTables with (nolock); --choose table from BI_feed
 
-	--Upload data into CapstoneDB.dbo.TnTech_TestResults
+	--Upload data into CapstoneDB.dbo.BI_HealthResults
 	INSERT INTO 
-		CapstoneDB.dbo.TnTech_TestResults(
+		CapstoneDB.dbo.BI_HealthResults(
 			TableName,
 			TestRunDate, 
 			TestName,
