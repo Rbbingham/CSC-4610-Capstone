9694f084f7850d147878c7b27d748811bd85699a
Robert Bingham <rbbingham1893@gmail.com>
2024-02-20 16:46:08 -0500

    updated to use table type

--- procs/BI_InsertTestResult.sql
+++ procs/BI_InsertTestResult.sql
@@ -1,49 +1,69 @@
 CREATE OR ALTER PROCEDURE [dbo].[BI_InsertTestResult]
-	@TableName VARCHAR(256),
-	@TestRunDate DATE,
-	@TestName VARCHAR(256),
-	@ActualResult BIGINT,
-	@ExpectedResult BIGINT,
-	@Deviation BIGINT,
-	@CreatedOn DATETIME,
-	@CreatedBy VARCHAR(256),
-	@ModifiedOn DATETIME,
-	@ModifiedBy VARCHAR(256)
+	@Table [dbo].[TnTech_TableType] READONLY
 AS
 BEGIN
 	SET NOCOUNT ON;
 
-	IF EXISTS ( SELECT * FROM CapstoneDB.dbo.BI_HealthResults WHERE TableName = @TableName AND TestName = @TestName AND TestRunDate = @TestRunDate)
+	IF EXISTS ( 
+		SELECT 
+			* 
+		FROM 
+			CapstoneDB.dbo.BI_HealthResults AS T1
+		INNER JOIN
+			@Table AS T2
+		ON 
+			T1.TableName = T2.TableName AND 
+			T1.TestName = T2.TestName AND 
+			T1.TestRunDate = T2.TestRunDate)
 	BEGIN
 		UPDATE CapstoneDB.dbo.BI_HealthResults
-		SET
-			[ActualResult] = @ActualResult,
-			[ExpectedResult] = @ExpectedResult,
-			[Deviation] = @Deviation,
-			[CreatedOn] = @CreatedOn,
-			[CreatedBy] = @CreatedBy,
-			[ModifiedOn] = @ModifiedOn,
-			[ModifiedBy] = @ModifiedBy
-		WHERE [TableName] = @TableName AND [TestName] = @TestName AND [TestRunDate] = @TestRunDate;
+			SET
+				T1.ActualResult = T2.ActualResult,
+				T1.ExpectedResult = T2.ExpectedResult,
+				T1.Deviation = T2.Deviation,
+				T1.CreatedOn = T2.CreatedOn,
+				T1.CreatedBy = T2.CreatedBy,
+				T1.ModifiedOn = T2.ModifiedOn,
+				T1.ModifiedBy = T2.ModifiedBy
+			FROM
+				[CapstoneDB].[dbo].[BI_HealthResults] AS T1
+			INNER JOIN
+				@Table AS T2
+			ON 
+				T1.TableName = T2.TableName AND 
+				T1.TestName = T2.TestName AND 
+				T1.TestRunDate = T2.TestRunDate;
 	END
 
 	ELSE
 
 	BEGIN
-		INSERT INTO CapstoneDB.dbo.BI_HealthResults (TableName, TestRunDate, TestName, ActualResult, ExpectedResult, Deviation, CreatedOn, CreatedBy, ModifiedOn, ModifiedBy)
-		VALUES (
-			@TableName,
-			@TestRunDate,
-			@TestName,
-			@ActualResult,
-			@ExpectedResult,
-			@Deviation,
-			@CreatedOn,
-			@CreatedBy,
-			@ModifiedOn,
-			@ModifiedBy
-			);
-	END
+		INSERT INTO 
+			CapstoneDB.dbo.TnTech_TestResults(
+				TableName,
+				TestRunDate, 
+				TestName,
+				ActualResult,
+				ExpectedResult,
+				Deviation,
+				CreatedOn,
+				CreatedBy,
+				ModifiedOn,
+				ModifiedBy)
+		SELECT
+			TableName,
+			TestRunDate,
+			TestName,
+			ActualResult,
+			ExpectedResult,
+			Deviation,
+			CreatedOn,
+			CreatedBy,
+			ModifiedOn,
+			ModifiedBy
+		FROM
+			@Table
+END
 
 	SET NOCOUNT OFF;
 END;
