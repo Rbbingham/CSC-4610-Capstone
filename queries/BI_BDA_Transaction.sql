9336558ce95c58459853e50f67ca72cc7a155e18
Carlos Escudero <Carlos.Escudero@transcard.com>
2024-02-22 17:16:36 -0500

    query testing continued

--- queries/BI_BDA_Transaction.sql
+++ queries/BI_BDA_Transaction.sql
@@ -1,9 +1,28 @@
+select top 10 
+		id,
+		cast (createdOn as date) as CreatedOn,
+		tranId,
+		adminNumber,
+		transactionAmount
+From BI_Feed.dbo.BI_BDA_Transactions
+order by CAST(CreatedOn as date ) desc;
+
+select count(cast(CreatedOn as Date))as CreatedOn
+from BI_Feed.dbo.BI_BDA_Transactions
+where CreatedOn > '2024-02-21';
+
 select  top 1 count(distinct CAST(createdOn as date) )as CreatedOn
 from BI_Feed.dbo.BI_BDA_Transactions (nolock)
 where createdOn > '2024-02-20'
 group by CAST(createdOn as date);
 
-select  top 1 Count(distinct CreatedOn)
+select top 1 cast(CreatedOn as date),
+			  Count(Cast(CreatedOn as date) )
 from BI_Feed.dbo.BI_BDA_Transactions with (nolock)
-where createdOn > '2024-02-20'
-group by createdOn;
+group by Cast(CreatedOn as date)
+order by Cast(CreatedOn as date);
+
+select tranId
+from BI_Feed.dbo.BI_BDA_Transactions
+where createdOn > '2024-02-21'
+order by tranID;
