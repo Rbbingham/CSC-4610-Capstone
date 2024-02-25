1196f58d0c001d80aaec328f3008f149216ba10c
Harrison Peloquin <Harrison.Peloquin@transcard.com>
2024-02-24 23:22:28 -0500

    testing

--- procs/BI_Call_Detail Proc.sql
+++ procs/BI_Call_Detail Proc.sql
@@ -54,6 +54,67 @@ BEGIN
 		'BI_Call_Detail' AS TableName,
 		 CAST(GETDATE() AS DATE) AS TestRunDate,
 		'Expected Call Count' AS TestName,
+		--------------------------------------------------------------------------------------
+		--------------------------------------------------------------------------------------
+		--------------------------------------------------------------------------------------
+		--------------------------------------------------------------------------------------
+		-- BI_Call_Detail Test (final updated dynamic query with averages from recent 365 days)
+		WITH WeeklyAverages AS (
+			SELECT 
+				timespan, 
+				AVG(ActualResult) as AverageResult
+			FROM (
+				SELECT -- For each day, calculate count of calls and compare against expected data
+					CAST(connectTime AS DATE) AS connectTime,
+					DATEPART(WEEKDAY, connectTime) AS day_of_week,
+					CASE
+						WHEN DATEPART(WEEKDAY, connectTime) IN (1,7) THEN 'Sat-Sun'
+						WHEN DATEPART(WEEKDAY, connectTime) IN (2,3,4,5) THEN 'Mon-Thu'
+						WHEN DATEPART(WEEKDAY, connectTime) = 6 THEN 'Fri'
+						ELSE 'NULL'
+					END as timespan,
+					COUNT(distinct callID) AS ActualResult
+				FROM BI_Feed.dbo.BI_Call_Detail WITH (nolock)
+				WHERE connectTime >= DATEADD(day, -365, GETDATE())
+				GROUP BY DATEPART(WEEKDAY, connectTime), CAST(connectTime AS DATE)
+			) AS subquery
+			GROUP BY timespan
+		),
+		DetailInfo AS (
+			SELECT -- For each day, calculate count of calls and compare against expected data
+				CAST(connectTime AS DATE) AS connectTime,
+				DATEPART(WEEKDAY, connectTime) AS day_of_week,
+				COUNT(distinct callID) AS ActualResult,
+				CASE
+					WHEN DATEPART(WEEKDAY, connectTime) IN (1,7) THEN 'Sat-Sun'
+					WHEN DATEPART(WEEKDAY, connectTime) IN (2,3,4,5) THEN 'Mon-Thu'
+					WHEN DATEPART(WEEKDAY, connectTime) = 6 THEN 'Fri'
+					ELSE 'NULL'
+				END as timespan
+			FROM BI_Feed.dbo.BI_Call_Detail WITH (nolock)
+			GROUP BY DATEPART(WEEKDAY, connectTime), CAST(connectTime AS DATE)
+		)
+		SELECT 
+			connectTime,
+			DetailInfo.timespan,
+			day_of_week,
+			AverageResult as ExpectedResult,
+			ActualResult,
+			CASE
+				WHEN ActualResult <= AverageResult THEN AverageResult - ActualResult
+				WHEN ActualResult > AverageResult THEN ActualResult - AverageResult
+			END as Deviation
+		FROM WeeklyAverages
+		FULL OUTER JOIN DetailInfo ON WeeklyAverages.timespan = DetailInfo.timespan
+		WHERE connectTime = DATEADD(day, -1, CAST(GETDATE() AS DATE))
+		GROUP BY connectTime, DetailInfo.timespan, day_of_week, AverageResult, ActualResult
+		ORDER BY connectTime DESC
+		--------------------------------------------------------------------------------------
+		--------------------------------------------------------------------------------------
+		--------------------------------------------------------------------------------------
+		--------------------------------------------------------------------------------------
+
+
 		COUNT(DISTINCT </*INSERT COLUMN*/>) AS ActualResult,
 		</*INSERT DEVIATION*/> AS ExpectedResult,
 		(COUNT(DISTINCT </*INSERT COLUMN*/>) - </*INSERT DEVIATION*/>) AS Deviation,
