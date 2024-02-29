6b9c1852d954df8172c2132a383b03d1e511af66
Carlos Escudero <Carlos.Escudero@transcard.com>
2024-02-29 17:08:48 -0500

    Testing on table continued

--- queries/BI_BDA_Transaction.sql
+++ queries/BI_BDA_Transaction.sql
@@ -1,28 +1,32 @@
-select top 10 
+select top 5 
 		id,
 		cast (createdOn as date) as CreatedOn,
 		tranId,
 		adminNumber,
 		transactionAmount
-From BI_Feed.dbo.BI_BDA_Transactions
+From BI_Feed.dbo.BI_BDA_Transactions with (nolock)
 order by CAST(CreatedOn as date ) desc;
 
 select count(cast(CreatedOn as Date))as CreatedOn
-from BI_Feed.dbo.BI_BDA_Transactions
-where CreatedOn > '2024-02-21';
+from BI_Feed.dbo.BI_BDA_Transactions with (nolock)
+where CreatedOn > '2024-02-28';
 
 select  top 1 count(distinct CAST(createdOn as date) )as CreatedOn
 from BI_Feed.dbo.BI_BDA_Transactions (nolock)
-where createdOn > '2024-02-20'
+where createdOn > '2024-02-28'
 group by CAST(createdOn as date);
 
-select top 1 cast(CreatedOn as date),
+select top 2 cast(CreatedOn as date) as CreatedOn,
 			  Count(Cast(CreatedOn as date) )
 from BI_Feed.dbo.BI_BDA_Transactions with (nolock)
 group by Cast(CreatedOn as date)
-order by Cast(CreatedOn as date);
+order by Cast(CreatedOn as date) Desc;
 
-select tranId
-from BI_Feed.dbo.BI_BDA_Transactions
-where createdOn > '2024-02-21'
-order by tranID;
+select Cast(CreatedOn as DATE) as CreatedOn,
+		count(distinct tranId) as TransactionCount
+from BI_Feed.dbo.BI_BDA_Transactions with (nolock)
+where createdOn > '2024-02-28'
+group by Cast(createdOn as Date);
+
+
+
