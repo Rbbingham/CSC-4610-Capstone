e4119f8d301a221cd06007d67add55c7627deb4e
Harrison Peloquin <Harrison.Peloquin@transcard.com>
2024-02-29 17:07:47 -0500

    bdatransactions

--- queries/BI_BDA_Transactions_query.sql
+++ queries/BI_BDA_Transactions_query.sql
@@ -7,10 +7,22 @@ FROM BI_BDA_Transactions;
 -- Finds averages of same day of each month
 USE BI_Feed
 
-SELECT DATEPART(day, transactionDate) as DayofMonth, AVG(transactionAmount) as avgamount
+SELECT timespan, AVG(transactionAmount) as avgamount
 FROM
 (
-	SELECT id, CAST(createdON as DATE) as transactionDate, tranID, adminNumber, transactionAmount
+	SELECT id, CAST(createdON as DATE) as transactionDate, tranID, adminNumber, transactionAmount,
+	CASE
+			WHEN DATEPART(WEEKDAY, createdOn) = 1 THEN 1
+			WHEN DATEPART(WEEKDAY, createdOn) = 2 THEN 2
+			WHEN DATEPART(WEEKDAY, createdOn) = 3 THEN 3
+			WHEN DATEPART(WEEKDAY, createdOn) = 4 THEN 4
+			WHEN DATEPART(WEEKDAY, createdOn) = 5 THEN 5
+			WHEN DATEPART(WEEKDAY, createdOn) = 6 THEN 6
+			WHEN DATEPART(WEEKDAY, createdOn) = 7 THEN 7
+			ELSE 'NULL'
+	END as timespan
 	FROM BI_BDA_Transactions
+	WHERE createdOn >= DATEADD(day, -365, GETDATE())
 ) AS subquery
-GROUP BY DATEPART(day, transactionDate)
+GROUP BY timespan
+ORDER BY timespan
