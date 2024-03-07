fa61b77f3de4cd0c120126f6cb80eded04049a97
cgcunningh42 <cgcunningh42@tntech.edu>
2024-03-07 15:22:32 -0600

    more probing for this table

--- queries/BI_BankCore_Funding_Transactions.sql
+++ queries/BI_BankCore_Funding_Transactions.sql
@@ -6,3 +6,15 @@ select Count(ModifedOnDate) as Amount from dbo.BI_BankCore_Funding_Transactions 
 select CAST(createdOn as DATE), count(ModifedOnDate) from dbo.BI_BankCore_Funding_Transactions with (nolock)
 group by CAST(CreatedOn as DATE)
 order by CAST(CreatedOn as DATE) DESC;
+
+CASE
+				WHEN DATEPART(DAY, CAST(transactionDate as DATE)) = 1 THEN 'FirstofMonth'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 1 THEN 'Sun'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 2 THEN 'Mon'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 3 THEN 'Tues'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 4 THEN 'Wed'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 5 THEN 'Thu'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 6 THEN 'Fri'
+				WHEN DATEPART(WEEKDAY, transactionDate) = 7 THEN 'Sat'
+				ELSE 'NULL'
+			END as timespan
