2b23d0a13a413b7baee9df641f00b11c9f809707
Harrison Peloquin <Harrison.Peloquin@transcard.com>
2024-03-06 19:59:50 -0500

    for carlos

--- queries/BI_PA_Transactions.sql
+++ queries/BI_PA_Transactions.sql
@@ -9,24 +9,57 @@ FROM BI_Feed.dbo.BI_PA_Transactions
 GROUP BY Cast(transactionDate as date)
 ORDER BY Cast(transactionDate as date) DESC;
 ------------------------------------------------------------------
-SELECT 
-	CAST(transactionDate as date) as TransactionDate,
-	COUNT(transactionReferenceId) as TransactionCount,
-	COUNT(distinct paymentAccountId) as PaymentAccountId,
-	DATEPART(Weekday, transactionDate) AS day_of_week,
+
+WITH WeeklyAverages as (
+	SELECT 
+		timespan, 
+		AVG(TransactionCount) as AverageResult
+	FROM (
+		SELECT 
+			CAST(transactionDate as date) as TransactionDate,
+			COUNT(transactionReferenceId) as TransactionCount,
+			DATEPART(Weekday, transactionDate) AS day_of_week,
+			CASE
+				WHEN DATEPART(WEEKDAY, transactionDate) in (1,7) THEN 'Sat-Sun'
+				WHEN DATEPART(WEEKDAY, transactionDate) IN (2,3,4,5,6) THEN 'Mon-Fri'
+				ELSE 'NULL'
+			END as timespan
+		FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
+		WHERE TransactionDate >= DATEADD(day, -365, GETDATE())
+		GROUP BY DATEPART(Weekday, transactionDate), CAST(transactionDate as date)
+		) AS subquery
+		GROUP BY timespan
+),
+DetailInfo as (
+	SELECT 
+		CAST(transactionDate as date) as TransactionDate,
+		COUNT(transactionReferenceId) as ActualResult,
+		DATEPART(Weekday, transactionDate) AS day_of_week,
+		CASE
+			WHEN DATEPART(WEEKDAY, transactionDate) in (1,7) THEN 'Sat-Sun'
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (2,3,4,5, 6) THEN 'Mon-Fri'
+			ELSE 'NULL'
+		END as timespan
+	FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
+	WHERE TransactionDate >= DATEADD(day, -365, GETDATE())
+	GROUP BY DATEPART(Weekday, transactionDate), CAST(transactionDate as date)
+)
+SELECT
+	TransactionDate,
+	DetailInfo.timespan,
+	day_of_week,
+	AverageResult as ExpectedResult,
+	ActualResult,
 	CASE
-		WHEN DATEPART(WEEKDAY, transactionDate) = 1 THEN 'Sun'
-		WHEN DATEPART(WEEKDAY, transactionDate) = 2 THEN 'Mon'
-		WHEN DATEPART(WEEKDAY, transactionDate) = 3 THEN 'Tues'
-		WHEN DATEPART(WEEKDAY, transactionDate) = 4 THEN 'Wed'
-		WHEN DATEPART(WEEKDAY, transactionDate) = 5 THEN 'Thu'
-		WHEN DATEPART(WEEKDAY, transactionDate) = 6 THEN 'Fri'
-		WHEN DATEPART(WEEKDAY, transactionDate) = 7 THEN 'Sat'
-		ELSE 'NULL'
-	END as timespan
-FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
-GROUP BY DATEPART(Weekday, transactionDate), CAST(transactionDate as date)
-ORDER BY Cast(transactionDate as date) DESC;
+		WHEN ActualResult <= AverageResult THEN AverageResult - ActualResult
+		WHEN ActualResult > AverageResult THEN ActualResult - AverageResult
+	END as Deviation
+FROM WeeklyAverages
+FULL OUTER JOIN DetailInfo ON WeeklyAverages.timespan = DetailInfo.timespan
+GROUP BY TransactionDate, DetailInfo.timespan, day_of_week, AverageResult, ActualResult
+ORDER BY TransactionDate DESC;
+
+
 -------------------------------------------------------------------
 
 WITH WeeklyAverages as (
