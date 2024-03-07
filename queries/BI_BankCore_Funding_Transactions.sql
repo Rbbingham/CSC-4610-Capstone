5b5b6b0143bf2d6faebd5bb41be1dbfd6a245805
Harrison Peloquin <Harrison.Peloquin@transcard.com>
2024-03-07 16:36:21 -0500

    for collin

--- queries/BI_BankCore_Funding_Transactions.sql
+++ queries/BI_BankCore_Funding_Transactions.sql
@@ -1,13 +1,36 @@
 use BI_Feed;
 
-select Count(ModifedOnDate) as Amount from dbo.BI_BankCore_Funding_Transactions with (nolock)
+select Count(ModifedOnDate) as Amount from dbo.BI_BankCore_Funding_Transactions with (nolock);
 --where ModifedOnDate = CAST(DATEADD(day, -1, GETDATE()) as DATE);
-
-select CAST(createdOn as DATE), count(ModifedOnDate) from dbo.BI_BankCore_Funding_Transactions with (nolock)
-group by CAST(CreatedOn as DATE)
+------------------------------------------------------------------------------------------------------
+select 
+	CAST(createdOn as DATE) as createdOn, 
+	count(ModifedOnDate),
+	DATEPART(Weekday, createdON) AS day_of_week,
+	CASE
+		--WHEN DATEPART(DAY, CAST(createdOn as DATE)) = 1 THEN 'FirstofMonth'
+		WHEN DATEPART(WEEKDAY, createdOn) = 1 THEN 'Sun'
+		WHEN DATEPART(WEEKDAY, createdOn) = 2 THEN 'Mon'
+		WHEN DATEPART(WEEKDAY, createdOn) = 3 THEN 'Tues'
+		WHEN DATEPART(WEEKDAY, createdOn) = 4 THEN 'Wed'
+		WHEN DATEPART(WEEKDAY, createdOn) = 5 THEN 'Thu'
+		WHEN DATEPART(WEEKDAY, createdOn) = 6 THEN 'Fri'
+		WHEN DATEPART(WEEKDAY, createdOn) = 7 THEN 'Sat'
+		ELSE 'NULL'
+	END as timespan
+from BI_Feed.dbo.BI_BankCore_Funding_Transactions with (nolock)
+group by CAST(CreatedOn as DATE), COUNT(ModifedOnDate)
 order by CAST(CreatedOn as DATE) DESC;
-
-CASE
+--------------------------------------------------------------------------------------------------
+	SELECT 
+		timespan, 
+		AVG(TransactionCount) as AverageResult
+	FROM (
+		SELECT 
+			CAST(transactionDate as date) as TransactionDate,
+			COUNT(transactionReferenceId) as TransactionCount,
+			DATEPART(Weekday, transactionDate) AS day_of_week,
+			CASE
 				WHEN DATEPART(DAY, CAST(transactionDate as DATE)) = 1 THEN 'FirstofMonth'
 				WHEN DATEPART(WEEKDAY, transactionDate) = 1 THEN 'Sun'
 				WHEN DATEPART(WEEKDAY, transactionDate) = 2 THEN 'Mon'
@@ -18,3 +41,8 @@ CASE
 				WHEN DATEPART(WEEKDAY, transactionDate) = 7 THEN 'Sat'
 				ELSE 'NULL'
 			END as timespan
+		FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
+		WHERE TransactionDate >= DATEADD(day, -183, GETDATE())
+		GROUP BY DATEPART(Weekday, transactionDate), CAST(transactionDate as date)
+		) AS subquery
+		GROUP BY timespan
