45c097d2febae15b83c286feb0dd3c5199107bd1
Robert Bingham <rbbingham1893@gmail.com>
2024-03-08 12:32:21 -0500

    Modified Toyota_Inventory to use InsertTestResult

--- procs/Toyota_Inventory.sql
+++ procs/Toyota_Inventory.sql
@@ -11,33 +11,17 @@ GO
 
 ******************************************************************************/
 
-CREATE OR ALTER PROCEDURE [dbo].[BI_Health_Toyota_Inventory] --name of procedure
+CREATE OR ALTER PROCEDURE [dbo].[BI_Health_Toyota_Inventory]
 AS 
 BEGIN
 	SET NOCOUNT ON;
 
-	IF OBJECT_ID('tempdb.dbo.temp_Toyota_Inventory') IS NOT NULL
-	BEGIN
-		DROP TABLE #temp_Toyota_Inventory-- temp table 
-	END;
-
-	--Create temp table 
-	CREATE TABLE #temp_Toyota_Inventory(
-		[TableName] varchar(256) NOT NULL,
-		[TestRunDate] date NOT NULL,
-		[TestName] varchar(256) NOT NULL,
-		[ActualResult] bigint NOT NULL,
-		[ExpectedResult] bigint NOT NULL,
-		[Deviation] bigint NOT NULL,
-		[CreatedOn] date NOT NULL,
-		[CreatedBy] varchar(256) NOT NULL,
-		[ModifiedOn] date NULL,
-		[ModifiedBy] varchar(256) NULL
-	);
+	-- create temp table 
+	DECLARE @temp_Toyota_Inventory AS [dbo].[TnTech_TableType];
 
 	-- run normal query into temp table
 	INSERT INTO 
-		#temp_Toyota_Inventory( --temp table name
+		@temp_Toyota_Inventory(
 			TableName,
 			TestRunDate, 
 			TestName,
@@ -62,34 +46,8 @@ BEGIN
 	FROM 
 		BI_Feed.dbo.Toyota_Inventory with (nolock); -- choose table from BI_feed
 
-	--Upload data into CapstoneDB.dbo.BI_HealthResults
-	INSERT INTO 
-		CapstoneDB.dbo.BI_HealthResults(
-			TableName,
-			TestRunDate, 
-			TestName,
-			ActualResult,
-			ExpectedResult,
-			Deviation,
-			CreatedOn,
-			CreatedBy,
-			ModifiedOn,
-			ModifiedBy)
-	SELECT
-		TableName,
-		TestRunDate, 
-		TestName,
-		ActualResult,
-		ExpectedResult,
-		Deviation,
-		CreatedOn,
-		CreatedBy,
-		ModifiedOn,
-		ModifiedBy
-	FROM 
-		#temp_Toyota_Inventory; --temp table 
-
-	DROP TABLE #temp_Toyota_Inventory;
+	-- upload data into CapstoneDB.dbo.BI_HealthResults
+	EXEC [dbo].[BI_InsertTestResult] @Table = @temp_Toyota_Inventory;
 
 	SET NOCOUNT OFF;
 END
