b563c281ba442b6495fdaa50605465969e588ca7
hgpeloquin <94129262+hgpeloquin@users.noreply.github.com>
2024-03-14 21:13:53 -0500

    Update BI_BDA_Transactions_query.sql

--- queries/BI_BDA_Transactions_query.sql
+++ queries/BI_BDA_Transactions_query.sql
@@ -181,3 +181,124 @@ FULL OUTER JOIN DayOfMonthAverages on DayOfMonthAverages.day_of_month = DetailIn
 WHERE transactionDate >= DATEADD(day, -183, GETDATE())
 GROUP BY transactionDate, dayofMonthAvgTranIDCount, weeklyAvgTranIDCount, ActualResult
 ORDER BY transactionDate DESC;
+-----------------------------------------------------------------------------------------------
+-- Create a temporary table for weekly averages
+CREATE TABLE #WeeklyAverages (
+	day_of_week NVARCHAR(20),
+	weeklyAvgTranIDCount DECIMAL(18,2)
+);
+
+INSERT INTO #WeeklyAverages
+SELECT day_of_week, AVG(ActualResult) as weeklyAvgTranIDCount
+FROM
+(
+	SELECT
+		CAST(createdOn AS DATE) AS createdOn,
+		CASE
+			WHEN DATEPART(WEEKDAY, createdOn) IN (1,2) THEN 'Sun-Mon'
+			WHEN DATEPART(WEEKDAY, createdOn) IN (3,4,5) THEN 'Tues-Thur'
+			WHEN DATEPART(WEEKDAY, createdOn) = 6 THEN 'Fri'
+			WHEN DATEPART(WEEKDAY, createdOn) = 7 THEN 'Sat'
+			ELSE 'NULL'
+		END as day_of_week,
+		COUNT(tranID) as ActualResult
+	FROM BI_Feed.dbo.BI_BDA_Transactions WITH (nolock)
+	WHERE createdOn >= DATEADD(day, -183, GETDATE())
+	GROUP BY CAST(createdOn AS DATE), DATEPART(WEEKDAY, createdOn)
+) AS subquery
+GROUP BY day_of_week;
+
+-- Create a temporary table for day of month averages
+CREATE TABLE #DayOfMonthAverages (
+	day_of_month NVARCHAR(20),
+	dayofMonthAvgTranIDCount DECIMAL(18,2)
+);
+
+INSERT INTO #DayOfMonthAverages
+SELECT day_of_month, AVG(ActualResult) as dayofMonthAvgTranIDCount
+FROM
+(
+	SELECT
+		CAST(createdOn AS DATE) AS createdOn,
+		CASE
+			WHEN DATEPART(day, CAST(createdOn as DATE)) = 2 THEN 'SecondofMonth'
+			WHEN DATEPART(day, CAST(createdOn as DATE)) = 9 THEN 'NinthofMonth'
+			ELSE 'NULL'
+		END as day_of_month,
+		COUNT(tranID) as ActualResult
+	FROM BI_Feed.dbo.BI_BDA_Transactions WITH (nolock)
+	WHERE createdOn >= DATEADD(day, -183, GETDATE())
+	GROUP BY CAST(createdOn AS DATE)
+) AS subquery
+GROUP BY day_of_month;
+
+-- Create a temporary table for detailed information
+CREATE TABLE #DetailInfo (
+	transactionDate DATE,
+	day_of_week NVARCHAR(20),
+	day_of_month NVARCHAR(20),
+	ActualResult INT
+);
+
+INSERT INTO #DetailInfo
+SELECT
+	CAST(createdOn as DATE) as transactionDate, 
+	CASE
+		WHEN DATEPART(WEEKDAY, createdOn) IN (1,2) THEN 'Sun-Mon'
+		WHEN DATEPART(WEEKDAY, createdOn) IN (3,4,5) THEN 'Tues-Thur'
+		WHEN DATEPART(WEEKDAY, createdOn) = 6 THEN 'Fri'
+		WHEN DATEPART(WEEKDAY, createdOn) = 7 THEN 'Sat'
+		ELSE 'NULL'
+	END as day_of_week,
+	CASE
+		WHEN DATEPART(day, CAST(createdOn as DATE)) = 2 THEN 'Sec'
+		WHEN DATEPART(day, CAST(createdOn as DATE)) = 9 THEN 'Nin'
+		ELSE 'NULL'
+	END as day_of_month,
+	COUNT(tranID) as ActualResult
+FROM BI_Feed.dbo.BI_BDA_Transactions WITH (nolock)
+WHERE createdOn >= DATEADD(day, -183, GETDATE())
+GROUP BY CAST(createdOn as DATE), DATEPART(WEEKDAY, createdOn);
+
+-- Create a temporary table for expected results
+CREATE TABLE #ExpectedCalculator (
+	transactionDate DATE,
+	ExpectedResult DECIMAL(18,2)
+);
+
+INSERT INTO #ExpectedCalculator
+SELECT
+	transactionDate,
+	CASE
+		WHEN DayOfMonthAverages.dayofMonthAvgTranIDCount = 'Sec' THEN (dayofMonthAvgTranIDCount + weeklyAvgTranIDCount) / 2
+		WHEN DayOfMonthAverages.dayofMonthAvgTranIDCount = 'Nin' THEN (dayofMonthAvgTranIDCount + weeklyAvgTranIDCount) / 2
+		ELSE weeklyAvgTranIDCount
+	END as ExpectedResult
+FROM #DetailInfo DetailInfo
+FULL OUTER JOIN #WeeklyAverages WeeklyAverages on WeeklyAverages.day_of_week = DetailInfo.day_of_week
+FULL OUTER JOIN #DayOfMonthAverages DayOfMonthAverages on DayOfMonthAverages.day_of_month = DetailInfo.day_of_month
+WHERE transactionDate >= DATEADD(day, -183, GETDATE())
+GROUP BY transactionDate;
+
+-- Select query with deviations
+SELECT
+	transactionDate,
+	ActualResult,
+	ExpectedResult,
+	CASE
+		WHEN ActualResult <= weeklyAvgTranIDCount THEN weeklyAvgTranIDCount - ActualResult
+		WHEN ActualResult > weeklyAvgTranIDCount THEN ActualResult - weeklyAvgTranIDCount
+	END as Deviation
+FROM #DetailInfo
+FULL OUTER JOIN #WeeklyAverages on #WeeklyAverages.day_of_week = #DetailInfo.day_of_week
+FULL OUTER JOIN #DayOfMonthAverages on #DayOfMonthAverages.day_of_month = #DetailInfo.day_of_month
+LEFT JOIN #ExpectedCalculator ON #ExpectedCalculator.transactionDate = #DetailInfo.transactionDate
+WHERE transactionDate >= DATEADD(day, -183, GETDATE())
+GROUP BY transactionDate, dayofMonthAvgTranIDCount, weeklyAvgTranIDCount, ActualResult
+ORDER BY transactionDate DESC;
+
+-- Drop temporary tables
+DROP TABLE #WeeklyAverages;
+DROP TABLE #DayOfMonthAverages;
+DROP TABLE #DetailInfo;
+DROP TABLE #ExpectedCalculator;
