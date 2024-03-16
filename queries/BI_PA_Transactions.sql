67bc80f3be358b24b2265a24df0befc93486b19f
Harrison Peloquin <Harrison.Peloquin@transcard.com>
2024-03-16 18:43:10 -0400

    working query

--- queries/BI_PA_Transactions.sql
+++ queries/BI_PA_Transactions.sql
@@ -140,8 +140,9 @@ ORDER BY TransactionDate DESC;
 -- COUNT(transactionReferenceId) as TransactionCount
 CREATE TABLE #DayOfMonthAvgs (
 	day_of_month NVARCHAR(20),
-	transCountAvg INT
+	transCountAvgMonth INT
 );
+
 INSERT INTO #DayOfMonthAvgs
 SELECT 
 	day_of_month, 
@@ -151,43 +152,106 @@ FROM
 	SELECT 
 		CAST(transactionDate as date) as transactionDate,
 		COUNT(transactionReferenceId) as transactionCount,
-		DATEPART(day, transactionDate) AS day_of_month
+		CASE
+			WHEN DATEPART(day, transactionDate) = 1 THEN 'First'
+			WHEN DATEPART(day, transactionDate) = 9 THEN 'Ninth'
+			ELSE 'Normal'
+		END as day_of_month
 	FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
-	WHERE TransactionDate >= DATEADD(day, -183, GETDATE())
+	WHERE TransactionDate >= DATEADD(day, -62, GETDATE())
 	GROUP BY CAST(transactionDate as date), DATEPART(day, transactionDate)
 	--ORDER BY CAST(transactionDate as date)
 ) AS subquery
 GROUP BY day_of_month;
 
+CREATE TABLE #WeeklyAverages (
+	timespan NVARCHAR(20),
+	transCountAvgWeek INT
+);
+
+INSERT INTO #WeeklyAverages
+SELECT 
+	timespan, 
+	AVG(TransactionCount) as transCountAvgWeek
+FROM 
+(
+	SELECT 
+		CAST(transactionDate as date) as transactionDate,
+		COUNT(transactionReferenceId) as transactionCount,
+		DATEPART(WEEKDAY, transactionDate) AS day_of_week,
+		CASE
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (1,7) THEN 'Sat-Sun'
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (2,6) THEN 'Mon,Fri'
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (3,4,5) THEN 'Tues-Thu'
+			ELSE 'NULL'
+		END as timespan
+	FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
+	WHERE TransactionDate >= DATEADD(day, -183, GETDATE())
+	GROUP BY CAST(transactionDate as date), DATEPART(WEEKDAY, transactionDate)
+	--ORDER BY CAST(transactionDate as date) DESC
+) AS subquery
+GROUP BY timespan;
 
 CREATE TABLE #DetailInfo (
 	transactionDate DATE,
 	ActualResult INT,
-	day_of_month INT
-)
+	day_of_month NVARCHAR(20),
+	timespan NVARCHAR(20)
+);
 
 INSERT INTO #DetailInfo
 SELECT 
 		CAST(transactionDate as date) as transactionDate,
 		COUNT(transactionReferenceId) as ActualResult,
-		DATEPART(day, transactionDate) AS day_of_month
+		CASE
+			WHEN DATEPART(day, transactionDate) = 1 THEN 'First'
+			WHEN DATEPART(day, transactionDate) = 9 THEN 'Ninth'
+			ELSE 'Normal'
+		END as day_of_month,
+		CASE
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (1,7) THEN 'Sat-Sun'
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (2,6) THEN 'Mon,Fri'
+			WHEN DATEPART(WEEKDAY, transactionDate) IN (3,4,5) THEN 'Tues-Thu'
+			ELSE 'NULL'
+		END as timespan
 FROM BI_Feed.dbo.BI_PA_Transactions with (nolock)
 WHERE TransactionDate >= DATEADD(day, -183, GETDATE())
-GROUP BY CAST(transactionDate as date), DATEPART(day, transactionDate)
---ORDER BY CAST(transactionDate as date)
+GROUP BY CAST(transactionDate as date), DATEPART(day, transactionDate), DATEPART(WEEKDAY, transactionDate)
+ORDER BY CAST(transactionDate as date) DESC;
 
+CREATE TABLE #ExpectedCalculator (
+	transactionDate DATE,
+	ExpectedResult INT
+);
 
+INSERT INTO #ExpectedCalculator
 SELECT
 	transactionDate,
+	CASE
+		WHEN #DayOfMonthAvgs.day_of_month IN ('First', 'Ninth') THEN transCountAvgWeek * 0.01 + transCountAvgMonth * 0.99
+		ELSE transCountAvgWeek * 0.99 + transCountAvgMonth * 0.01
+	END as ExpectedResult
+FROM #DetailInfo
+FULL OUTER JOIN #WeeklyAverages on #WeeklyAverages.timespan = #DetailInfo.timespan
+FULL OUTER JOIN #DayOfMonthAvgs on #DayOfMonthAvgs.day_of_month = #DetailInfo.day_of_month
+WHERE transactionDate >= DATEADD(day, -183, GETDATE())
+GROUP BY transactionDate, #DayOfMonthAvgs.day_of_month, #WeeklyAverages.timespan, transCountAvgMonth, transCountAvgWeek;
+
+SELECT
+	#DetailInfo.transactionDate,
 	#DetailInfo.day_of_month,
-	transCountAvg as ExpectedResult,
+	ExpectedResult,
 	ActualResult,
-	ABS(transCountAvg - ActualResult) as Deviation
+	ABS(ExpectedResult - ActualResult) as Deviation
 FROM #DetailInfo
 FULL OUTER JOIN #DayOfMonthAvgs ON #DetailInfo.day_of_month = #DayOfMonthAvgs.day_of_month
-WHERE transactionDate >= DATEADD(day, -183, GETDATE())
-GROUP BY transactionDate, #DetailInfo.day_of_month, transCountAvg, ActualResult
+FULL OUTER JOIN #WeeklyAverages ON #DetailInfo.timespan = #WeeklyAverages.timespan
+FULL OUTER JOIN #ExpectedCalculator ON #ExpectedCalculator.transactionDate = #DetailInfo.transactionDate
+WHERE #DetailInfo.transactionDate >= DATEADD(day, -183, GETDATE())
+GROUP BY #DetailInfo.transactionDate, #DetailInfo.day_of_month, ExpectedResult, ActualResult
 ORDER BY transactionDate DESC;
 
 DROP TABLE #DayOfMonthAvgs;
+DROP TABLE #WeeklyAverages
+DROP TABLE #ExpectedCalculator
 DROP TABLE #DetailInfo;
