e79d3c68f20a3d1ed9f04ed7560361a9efb96c29
Carlos Escudero <Carlos.Escudero@transcard.com>
2024-03-16 16:23:29 -0400

    Basic Data gathering

--- queries/BI_BDA_Partners_query.sql
+++ queries/BI_BDA_Partners_query.sql
@@ -2,3 +2,18 @@ SELECT *
 FROM BI_Feed.dbo.BI_BDA_Partners
 ORDER BY id DESC;
 
+select * from BI_BDA_Partners order by id Desc;
+
+select Count(partnerId) from BI_BDA_Partners
+select Count(Distinct partnerId) from BI_BDA_Partners
+
+select Count(partnerName) from BI_BDA_Partners
+select Count(Distinct partnerName) from BI_BDA_Partners
+
+select Count(*) from BI_BDA_Partners
+
+Select Count(productId) from BI_BDA_Partners with(nolock)
+Select Count(Distinct productId) from BI_BDA_Partners with(nolock)
+
+SELECT COUNT(ID)from BI_BDA_Partners with(nolock)
+SELECT COUNT(DISTINCT ID)from BI_BDA_Partners with (nolock)
