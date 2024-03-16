1f76abf47df8b04e3135e2bd624212dca4cf20f8
Harrison Peloquin <Harrison.Peloquin@transcard.com>
2024-03-16 16:39:33 -0400

    avgs

--- queries/BI_BDA_Partners_query.sql
+++ queries/BI_BDA_Partners_query.sql
@@ -18,21 +18,24 @@ Select Count(Distinct productId) from BI_BDA_Partners with(nolock)
 SELECT COUNT(ID)from BI_BDA_Partners with(nolock)
 SELECT COUNT(DISTINCT ID)from BI_BDA_Partners with (nolock)
 -------------------------------------------------------------------
-
-SELECT
-	CAST(createdOn AS DATE) AS createdOn,
-	CASE
-		WHEN DATEPART(WEEKDAY, createdOn) = 1 THEN 'Sun'
-		WHEN DATEPART(WEEKDAY, createdOn) = 2 THEN 'Mon'
-		WHEN DATEPART(WEEKDAY, createdOn) = 3 THEN 'Tues'
-		WHEN DATEPART(WEEKDAY, createdOn) = 4 THEN 'Wed'
-		WHEN DATEPART(WEEKDAY, createdOn) = 5 THEN 'Thur'
-		WHEN DATEPART(WEEKDAY, createdOn) = 6 THEN 'Fri'
-		WHEN DATEPART(WEEKDAY, createdOn) = 7 THEN 'Sat'
-		ELSE 'NULL'
-	END as day_of_week,
-	COUNT(ID) as IDCount
-FROM BI_Feed.dbo.BI_BDA_Partners
-WHERE createdOn >= DATEADD(day, -365, CAST(GETDATE() AS DATE))
-GROUP BY CAST(createdOn AS DATE), DATEPART(WEEKDAY, createdOn)
-ORDER BY CAST(createdOn AS DATE) DESC;
+SELECT day_of_week, AVG(IDCount) as weeklyIDCountAvg
+FROM 
+(
+	SELECT
+		CAST(createdOn AS DATE) AS createdOn,
+		CASE
+			WHEN DATEPART(WEEKDAY, createdOn) = 1 THEN 'Sun'
+			WHEN DATEPART(WEEKDAY, createdOn) = 2 THEN 'Mon'
+			WHEN DATEPART(WEEKDAY, createdOn) = 3 THEN 'Tues'
+			WHEN DATEPART(WEEKDAY, createdOn) = 4 THEN 'Wed'
+			WHEN DATEPART(WEEKDAY, createdOn) = 5 THEN 'Thur'
+			WHEN DATEPART(WEEKDAY, createdOn) = 6 THEN 'Fri'
+			WHEN DATEPART(WEEKDAY, createdOn) = 7 THEN 'Sat'
+			ELSE 'NULL'
+		END as day_of_week,
+		COUNT(ID) as IDCount
+	FROM BI_Feed.dbo.BI_BDA_Partners
+	WHERE createdOn >= DATEADD(day, -365, CAST(GETDATE() AS DATE))
+	GROUP BY CAST(createdOn AS DATE), DATEPART(WEEKDAY, createdOn)
+) as subquery
+GROUP BY day_of_week;
